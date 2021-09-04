bcbf07a06536e35526f8da56fc474c1796755eec
Quentin Adatte <quentin.ad@outlook.com>
2021-09-04 19:30:52 +0200

    Push for experiments with soft

---
+++ datascope/algorithms/__init__.py
@@ -0,0 +1,5 @@
+from .Measure import Measure
+from .KNN_Shapley import KNN_Shapley
+from .KNN_Hard_Shapley import KNN_Hard_Shapley
+from .KNN_Soft_Shapley import KNN_Soft_Shapley
+from .TMC_Shapley import TMC_Shapley
