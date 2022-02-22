0b78e8a047382ca028e7bcc906698192954f8d50
Bojan Karlaš <bojan.karlas@gmail.com>
2022-02-22 19:25:51 +0100

    Small fix in experiment run scripts.

--- scripts/run-data-discard-small-balancedval.sh
+++ scripts/run-data-discard-small-balancedval.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-METHOD=random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100
 python -m experiments run \
     --scenario data-discard \
     --dataset UCI \
-    --method  ${METHOD} \
+    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
     --trainsize 1000 \
     --valsize 500
