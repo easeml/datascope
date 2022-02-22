dd8c14f625bb7b81e385a2a7ecb8ca5c38bc5482
Bojan Karlaš <bojan.karlas@gmail.com>
2022-02-22 19:16:48 +0100

    Add script to run the data discard experiment.

---
+++ scripts/run-data-discard-small-balancedval.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+METHOD=random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100
+python -m experiments run \
+    --scenario data-discard \
+    --dataset UCI \
+    --method  ${METHOD} \
+    --trainsize 1000 \
+    --valsize 500
