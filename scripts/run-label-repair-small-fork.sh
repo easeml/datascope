08ae29858d01e564e68afa3a458b977e33c92a17
Bojan Karlaš <bojan.karlas@gmail.com>
2022-02-25 08:48:38 +0000

    Add dirty ration config to fork experiment runner script.

--- scripts/run-label-repair-small-fork.sh
+++ scripts/run-label-repair-small-fork.sh
@@ -5,4 +5,5 @@ python -m experiments run \
     --method random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
     --trainsize 1000 \
     --valsize 500 \
-    --providers 100
+    --providers 100 \
+    --dirtyratio 1.0
