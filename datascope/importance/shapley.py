Fix bug with shapley computation.

datascope/importance/shapley.py
@@ -140,12 +140,12 @@ def compute_all_importances(unit_distances: ndarray, unit_utilities: ndarray) ->
     unit_utilities = np.vstack((unit_utilities, np.ones((1, n_test)) * 0.5))
     for j in prange(n_test):
         idxs = np.append(np.argsort(unit_distances[:, j]), [n_units])
+        current = 0.0
         for i in prange(n_units - 1, -1, -1):
             i_1 = idxs[i]
             i_2 = idxs[i + 1]
-            all_importances[i_1] += all_importances[i_2] + (unit_utilities[i_1, j] - unit_utilities[i_2, j]) / float(
-                i + 1
-            )
+            current = (current + (unit_utilities[i_1, j] - unit_utilities[i_2, j])) / float(i + 1)
+            all_importances[i_1] += current
         # all_importances /= n_units
     return all_importances[:-1] / n_test
 
@@ -199,7 +199,7 @@ def compute_shapley_1nn_mapfork(
     #             unit_utilities[idxs[i], j] - unit_utilities[idxs[i + 1], j]
     #         ) / float(i + 1)
 
-    all_importances = compute_all_importances_cy(unit_distances, unit_utilities)
+    all_importances = compute_all_importances(unit_distances, unit_utilities)
 
     # Aggregate results.
     # importances = np.mean(all_importances[:-1], axis=1)