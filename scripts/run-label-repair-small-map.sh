52934b8aa59bde5a7c54de4ae3a098d8d472bd72
Bojan Karlaš <bojan.karlas@gmail.com>
2023-01-06 23:24:58 +0100

    Add the regular TMC Shapley methods to label repair small scripts.

--- scripts/run-label-repair-small-map.sh
+++ scripts/run-label-repair-small-map.sh
@@ -2,7 +2,7 @@
 
 CMD="python -m datascope.experiments run \
     --scenario label-repair \
-    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
+    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-010 shapley-tmc-100 shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
     --repairgoal accuracy \
     --trainsize 1000 \
     --valsize 500 \
