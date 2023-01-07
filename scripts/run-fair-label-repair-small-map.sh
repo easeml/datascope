618d7861b4dc32acbc9c214a0a9fe44d88758359
Bojan Karlaš <bojan.karlas@gmail.com>
2023-01-07 17:00:54 +0100

    Fix the fair label repair script.

--- scripts/run-fair-label-repair-small-map.sh
+++ scripts/run-fair-label-repair-small-map.sh
@@ -2,11 +2,11 @@
 
 CMD="python -m datascope.experiments run \
     --scenario label-repair \
-    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
+    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-010 shapley-tmc-100 shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
     --repairgoal fairness \
     --trainsize 1000 \
     --valsize 500 \
-    --testsize 500"
+    --testsize 500 "
 
 CMD+=" ${@}"
 
