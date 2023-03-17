c36002551e4303ce6e1429c7485b2074fa25b3d7
Bojan Karlaš <bojan.karlas@gmail.com>
2023-03-16 23:21:19 -0400

    Add resnet-18 as a model to experiments.

--- experiments/datascope/experiments/pipelines/models.py
+++ experiments/datascope/experiments/pipelines/models.py
@@ -1,4 +1,11 @@
+import numpy as np
+import tempfile
+import torch
+import torch.utils.data
+
 from enum import Enum
+from numpy.typing import NDArray
+from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.linear_model import LogisticRegression
@@ -6,6 +13,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC, LinearSVC
+from sklearn.utils.multiclass import unique_labels
+from transformers import AutoImageProcessor, ResNetForImageClassification, TrainingArguments, Trainer
 from typing import Dict
 from xgboost import XGBClassifier
 
@@ -21,6 +30,8 @@ class ModelType(str, Enum):
     NaiveBayes = "nb"
     NeuralNetwork = "nn"
     XGBoost = "xgb"
+    ResNet18 = "resnet-18"
+    MiniLM = "mini-lm"
 
 
 KEYWORD_REPLACEMENTS: Dict[str, str] = {
@@ -33,7 +44,70 @@ KEYWORD_REPLACEMENTS: Dict[str, str] = {
     ModelType.NaiveBayes.value: "Naive Bayes Classifier",
     ModelType.NeuralNetwork.value: "Neural Network",
     ModelType.XGBoost.value: "XGBoost",
+    ModelType.ResNet18.value: "ResNet-18",
+    ModelType.MiniLM.value: "MiniLM",
 }
+
+
+class TorchDataset(torch.utils.data.Dataset):
+    def __init__(self, X: NDArray, y: NDArray, feature_extractor=None):
+        if X.ndim == 3:
+            X = np.expand_dims(X, axis=X.ndim)
+        if X.shape[-1] == 1:
+            X = np.tile(X, (1, 1, 1, 3))
+        self.X = X
+        self.y = y
+        self.feature_extractor = feature_extractor
+
+    def __getitem__(self, idx):
+        if self.feature_extractor is not None:
+            result = self.feature_extractor(self.X[idx], return_tensors="pt")
+            result["pixel_values"] = torch.squeeze(result["pixel_values"])
+            result["labels"] = torch.tensor(self.y[idx])
+            return result
+        else:
+            return {"pixel_values": torch.tensor(self.X[idx]), "labels": torch.tensor(self.y[idx])}
+
+    def __len__(self):
+        return len(self.y)
+
+
+class ResNet18Classifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, n_epochs: int = 5) -> None:
+        self.n_epochs = n_epochs
+        self.cuda_mode = torch.cuda.is_available()
+
+    def fit(self, X: NDArray, y: NDArray) -> None:
+        self.feature_extractor = AutoImageProcessor.from_pretrained("microsoft/resnet-18")
+        self.model = ResNetForImageClassification.from_pretrained(
+            "microsoft/resnet-18", num_labels=2, ignore_mismatched_sizes=True
+        )
+        if self.cuda_mode:
+            self.model = self.model.to("cuda:0")
+        self.classes_ = unique_labels(y)
+        self.train_dataset = TorchDataset(X, y, self.feature_extractor)
+        self.tempdir = tempfile.mkdtemp(prefix="resnet18")
+        self.training_args = TrainingArguments(output_dir=self.tempdir, num_train_epochs=self.n_epochs)
+        self.trainer = Trainer(
+            model=self.model,
+            args=self.training_args,
+            train_dataset=self.train_dataset,
+        )
+        self.trainer.train()
+
+    def predict(self, X: NDArray) -> NDArray:
+        if X.ndim == 3:
+            X = np.expand_dims(X, axis=X.ndim)
+        if X.shape[-1] == 1:
+            X = np.tile(X, (1, 1, 1, 3))
+        inputs = self.feature_extractor(list(X), return_tensors="pt")
+        if self.cuda_mode:
+            inputs = inputs.to("cuda:0")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        if self.cuda_mode:
+            outputs = outputs.cpu()
+        return outputs.logits.argmax(axis=1).numpy()
 
 
 def get_model(model_type: ModelType, **kwargs):
@@ -81,6 +155,9 @@ def get_model(model_type: ModelType, **kwargs):
         )
     elif model_type == ModelType.XGBoost:
         model = XGBClassifier(nthread=1, eval_metric="logloss")
+    elif model_type == ModelType.ResNet18:
+        n_epochs = kwargs.get("n_epochs", 5)
+        model = ResNet18Classifier(n_epochs=n_epochs)
     else:
         raise ValueError("Unknown model type '%s'." % str(model_type))
     return model
