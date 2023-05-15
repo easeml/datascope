6a133e31f63d29004c7d576c2d21a4a9a117f354
Bojan Karlaš <bojan.karlas@gmail.com>
2023-05-15 01:55:23 -0400

    Add augmentation script and add shapley-knn-raw to the report script.

--- scripts/generate-report-accuracy-2.sh
+++ scripts/generate-report-accuracy-2.sh
@@ -5,8 +5,8 @@ CMD="python -m datascope.experiments report \
         --index repaired_rel \
         --xtickfmt percent \
         --compare method \
-        --compareorder random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 shapley-tmc-010 shapley-tmc-100 \
-        --colors random:blue shapley-knn-single:red shapley-knn-interactive:yellow shapley-tmc-pipe-010:green shapley-tmc-pipe-100:brown shapley-tmc-010:purple shapley-tmc-100:cyan \
+        --compareorder random shapley-knn-single shapley-knn-raw shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 shapley-tmc-010 shapley-tmc-100 \
+        --colors random:blue shapley-knn-single:red shapley-knn-raw:pink shapley-knn-interactive:yellow shapley-tmc-pipe-010:green shapley-tmc-pipe-100:brown shapley-tmc-010:purple shapley-tmc-100:cyan \
         --targetval accuracy \
         --sliceby iteration \
         --sliceop max \
