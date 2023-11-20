a73de51dc5d87c801ddd78b9770cee2922494ded
Bojan Karlaš <bojan.karlas@gmail.com>
2023-11-19 20:17:42 -0500

    Update data discard script for biased data.

--- scripts/run-data-discard-small-biasedval.sh
+++ scripts/run-data-discard-small-biasedval.sh
@@ -3,13 +3,17 @@
 CMD="python -m datascope.experiments run \
     --scenario data-discard \
     --dataset UCI FolkUCI \
-    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
-    --repairgoal fairness \
+    --model logreg knn xgb \
+    --method random shapley-knn-single shapley-knn-interactive shapley-tmc-010 shapley-tmc-100 shapley-tmc-pipe-010 shapley-tmc-pipe-100 \
+    --repairgoal accuracy \
+    --utility acc \
     --trainsize 1000 \
     --valsize 500 \
     --testsize 500 \
     --trainbias 0.8 \
-    --valbias 0.8"
+    --valbias 0.5 \
+    --biasmethod label \
+    --maxremove 0.9 "
 
 CMD+=" ${@}"
 
